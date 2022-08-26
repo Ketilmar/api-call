e813cd40ae02240b9988e8399685bbae2dd6cb98
Ketil Kodehode <ketilmar.kodehode@gmail.com>
2022-08-26 19:01:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import UsingAxios from "./components/axiosCaller";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>Learn React</a>
+      </header>
+      <UsingAxios />
+    </div>
+  );
+}
+
+export default App;
