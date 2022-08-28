82e7136bb9625bd68b4d0ce18ba87aadfc457a71
Ketil Markhus <ketil@keebit.com>
2022-08-28 18:35:40 +0200

    fixed the response data. Now ready for hand in

--- src/components/axiosCaller.js
+++ src/components/axiosCaller.js
@@ -3,12 +3,12 @@ import React, { useEffect, useState } from "react";
 import "./axiosCaller.css";
 
 const UsingAxios = () => {
-  const [cats, setUsers] = useState([]);
+  const [cats, setState] = useState([]);
 
   const fetchData = () => {
     axios.get("https://catfact.ninja/facts").then((response) => {
-      setUsers(response.data);
-      //   console.log(response.data);
+      setState(response.data.data); // slet en stund her da jeg satt et objekt til setState (response.data) i stedet for et array (response.data.data)
+      console.log(response.data);
     });
   };
 
@@ -16,25 +16,14 @@ const UsingAxios = () => {
     fetchData();
   }, []);
 
-  let page = cats.data.map((facts, index) => {
-    return <li key={index}>{facts.fact}</li>;
+  let page = cats.map((facts, index) => {
+    return <li key={index}>{facts.fact}, </li>;
   });
-  console.log(page);
 
-  console.log(cats);
+  // console.log(page);
+  // console.log(cats);
+
   return <div className="result-body">{page}</div>;
 };
 
 export default UsingAxios;
-
-// {cats.data.map((facts, index) => {
-//     <li key={index}>{facts.fact}</li>;
-//   })}
-
-// {cats.data > 0 && (
-//     <ul>
-//       {cats.map((catFacts) => (
-//         <li key={catFacts.id}>{catFacts.name}</li>
-//       ))}
-//     </ul>
-//   )}
