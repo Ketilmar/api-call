617a91d31c56d17e1f555d798e1b836d3857be92
Ketil Markhus <ketil@keebit.com>
2022-08-29 00:38:22 +0200

    added some comments and some tests

--- src/components/axiosCaller.js
+++ src/components/axiosCaller.js
@@ -2,26 +2,27 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./axiosCaller.css";
 
-const UsingAxios = () => {
+const UsingAxios = ({ tabtext }) => {
   const [cats, setState] = useState([]);
 
   const fetchData = () => {
     axios.get("https://catfact.ninja/facts").then((response) => {
-      setState(response.data.data); // slet en stund her da jeg satt et objekt til setState (response.data) i stedet for et array (response.data.data)
-      console.log(response.data);
+      setState(response.data.data); // slet en stund her og skrev om stort sett alt, da jeg satt setState til et object (response.data) i stedet for et array (response.data.data)
+      console.log(response);
     });
   };
 
+  // only reason i see to use useEffect here, is to control when fetchData runs (ie at initial rendering). Else It will loop forever because it is used with useState
   useEffect(() => {
     fetchData();
+    document.title = `${tabtext}`;
   }, []);
+
+  // console.log(cats);
 
   let page = cats.map((facts, index) => {
     return <li key={index}>{facts.fact}, </li>;
   });
-
-  // console.log(page);
-  // console.log(cats);
 
   return <div className="result-body">{page}</div>;
 };
