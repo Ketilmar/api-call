0bc619163315221ecefc269fa8dbfc6848b247c5
Ketil Markhus <ketil@keebit.com>
2022-08-29 09:24:52 +0200

    added error handling to axios call

--- src/components/axiosCaller.js
+++ src/components/axiosCaller.js
@@ -5,11 +5,22 @@ import "./axiosCaller.css";
 const UsingAxios = ({ tabtext }) => {
   const [cats, setState] = useState([]);
 
-  const fetchData = () => {
-    axios.get("https://catfact.ninja/facts").then((response) => {
-      setState(response.data.data); // slet en stund her og skrev om stort sett alt, da jeg satt setState til et object (response.data) i stedet for et array (response.data.data)
-      console.log(response);
-    });
+  const fetchData = async () => {
+    await axios
+      .get("https://catfact.ninja/facts")
+      .then((response) => {
+        setState(response.data.data); // slet en stund her og skrev om stort sett alt, da jeg satt setState til et object (response.data) i stedet for et array (response.data.data)
+        console.log(response);
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+        } else if (err.request) {
+          console.log(err.request);
+        } else {
+          console.log("something happened");
+        }
+      });
   };
 
   // only reason i see to use useEffect here, is to control when fetchData runs (ie at initial rendering). Else It will loop forever because it is used with useState
